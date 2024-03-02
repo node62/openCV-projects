a3a2421a62382dbf2377f2123c9bdea08cb9085f
node62 <theoregi61@gmail.com>
2024-03-02 18:06:00 +0530

    edited the code

--- line_following/test1.py
+++ line_following/test1.py
@@ -2,20 +2,19 @@ import cv2
 import numpy as np
 import math
 
-cap = cv2.VideoCapture(r'C:\Users\Theodore Regimon\PycharmProjects\pythonProject\A Line Following Algorithm\finalSampleLine.mp4')
+cap = cv2.VideoCapture(r'C:\Users\Theodore Regimon\PycharmProjects\pythonProject\A Line Following Algorithm\cropLineSample.mp4')
 kernel = np.ones((5,5),np.uint8)
 tp=10
 l1=0
 width = int(cap.get(3))
 height = int(cap.get(4))
 
-# fourcc = cv2.VideoWriter_fourcc(*'XVID')
-# output_video = cv2.VideoWriter('output_video.mp4', fourcc, 30, (width, height))
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+output_video = cv2.VideoWriter('output_video.mp4', fourcc, 30, (width, height))
 
 def findMidPoint(img, y2, m1, m2):
     print(f"initial edge points: {m1}, {m2}")
     mat = np.array(img)
-
     x1=0
     x2=width-1
     ten=15
@@ -56,6 +55,18 @@ def findMidPoint(img, y2, m1, m2):
     print(f"final edge points: {m1}, {m2}")
     return mid, m1, m2
 
+def getContours(img, imgOg):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    max=0
+    mcnt=0
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > max:
+            max = area
+            mcnt = cnt
+    cv2.drawContours(imgOg, mcnt, -1, (0, 0,255), 1)
+    return imgOg
+
 a1=0
 a2=width-1
 b1=0
@@ -63,13 +74,16 @@ b2=width-1
 iter=0
 while True:
     _, imgOg = cap.read()
+    blank=np.zeros_like(imgOg)
     img = imgOg.copy()
     img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-    img = cv2.GaussianBlur(img, (3, 3), 0)
-    imgDilation = cv2.dilate(img, (3,3), iterations=1)
-    imgErosion = cv2.erode(img, (3,3), iterations=1)
-    img = imgCanny = cv2.Canny(img, 0, 160)
+    ret, img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY_INV)
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    img = cv2.dilate(img, (3,3), iterations=1)
+    img = cv2.erode(img, (3,3), iterations=1)
+    # img = imgCanny = cv2.Canny(img, 0, 160)
+    img = getContours(img, img)
+    contimg = getContours(img, blank)
     p1,a1,a2  = findMidPoint(img, round(0.6*height), a1, a2)
     print(f"P1: {p1}", )
     p2, b1, b2 = findMidPoint(img, round(0.9*height), b1, b2)
@@ -86,13 +100,16 @@ while True:
     if iter%100==0:
         l1=label
     cv2.putText(imgOg, str(angVel), (5, height-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2, cv2.LINE_AA)
-
+    # direction="Right"
+    # if p1 < p2:
+    #     direction="Left"
+    # cv2.putText(imgOg, direction, (5, height - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
     cv2.imshow("Video", imgOg)
-    cv2.imshow("canny", imgCanny)
-    # output_video.write(imgOg)
-    if cv2.waitKey(tp)&0xFF == ord(' '):
+    cv2.imshow("canny", contimg)
+    output_video.write(imgOg)
+    if cv2.waitKey(twhap)&0xFF == ord(' '):
         break
     iter=iter+1
-# cap.release()
-# output_video.release()
+cap.release()
+output_video.release()
 cv2.destroyAllWindows()
